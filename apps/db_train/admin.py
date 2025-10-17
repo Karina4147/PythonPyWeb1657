ebd1afaf93333bdd2076311e3dc828d9357cbad2
Карина <karina41471@gmail.com>
2025-10-17 11:53:13 +0300

    2

--- apps/db_train/admin.py
+++ apps/db_train/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-
-from django.contrib import admin
 from .models import Author
+from .models import AuthorProfile
+from .models import Entry
+from .models import Tag
 
+admin.site.register(AuthorProfile)
 admin.site.register(Author)
+admin.site.register(Entry)
+admin.site.register(Tag)
--- apps/db_train/models.py
+++ apps/db_train/models.py
@@ -120,3 +120,41 @@ class Author(models.Model):
         verbose_name = "Автор"
         verbose_name_plural = "Авторы"
 
+class AuthorProfile(models.Model):
+    author = models.OneToOneField('Author', on_delete=models.CASCADE)
+    stage = models.IntegerField(default=0,
+                                blank=True,
+                                verbose_name="Стаж",
+                                help_text="Стаж в годах")
+
+    def __str__(self):
+        return f"Автор: {self.author}; Стаж: {self.stage} лет"
+
+    class Meta:
+        verbose_name = "Профиль автора"
+        verbose_name_plural = "Профили авторов"
+
+class Entry(models.Model):
+    text = models.TextField(verbose_name="Текст статьи",
+                            )
+    author = models.ForeignKey("Author", on_delete=models.CASCADE, related_name='entries')
+    tags = models.ManyToManyField("Tag", related_name='entries')
+
+    def __str__(self):
+        return f"Текст статьи: {self.text} Автор: {self.author}"
+
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name="Название",
+                            )
+
+    def __str__(self):
+        return f"Название: {self.name}"
+
+    class Meta:
+        verbose_name = "Тэг"
+        verbose_name_plural = "Тэги"
