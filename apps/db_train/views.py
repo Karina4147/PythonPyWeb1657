5f55a0af85ad7200707f1e80bb95c0d787ea5e5a
Карина <karina41471@gmail.com>
2025-10-23 21:21:27 +0300

    4

--- apps/db_train/views.py
+++ apps/db_train/views.py
@@ -20,15 +20,13 @@ class TrainView(View):
         self.answer2 = results  # TODO Какой автор имеет наибольшее количество опубликованных статей?
         entries = Entry.objects.filter(Q(tags__name="Кино") | Q(tags__name="Музыка"))
         self.answer3 = entries  # TODO Какие статьи содержат тег 'Кино' или 'Музыка' ?
-        count_author = Author.objects.count
-        self.answer4 = count_author  # TODO Сколько авторов женского пола зарегистрировано в системе?
+        self.answer4 = Author.objects.filter(gender='ж').count()
         self.answer5 = None  # TODO Какой процент авторов согласился с правилами при регистрации?
-        self.answer6 = AuthorProfile.objects.filter(stage__range=(1, 5))  # TODO Какие авторы имеют стаж от 1 до 5 лет?
-        max_age = Author.objects.aggregate(max_age=Max('age'))
-        self.answer7 = max_age  # TODO Какой автор имеет наибольший возраст?
-        self.answer8 = None  # TODO Сколько авторов указали свой номер телефона?
+        self.answer6 = AuthorProfile.objects.filter(stage__range=(1, 5))
+        self.answer7 = Author.objects.aggregate(max_age=Max('age'))  # TODO Какой автор имеет наибольший возраст?
+        self.answer8 = Author.objects.filter(phone_number=True).count()  # TODO Сколько авторов указали свой номер телефона?
         self.answer9 = Author.objects.filter(age__lt=25)  # TODO Какие авторы имеют возраст младше 25 лет?
-        self.answer10 = None  # TODO Сколько статей написано каждым автором?
+        self.answer10 = Author.objects.annotate(entries_count=Count('entries')).values('username', 'entries_count')  # TODO Сколько статей написано каждым автором?
 
         context = {f'answer{index}': self.__dict__[f'answer{index}'] for index in range(1, 11)}
 
